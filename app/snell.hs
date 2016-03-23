20346bbc3806f4a53931c2e2bf6cbd510f3983f9
Wouter Oosterveld <wouter@fizzyflux.nl>
2016-03-23 20:26:54 +0100

    linear V3 instead of tuples

--- app/snell.hs
+++ app/snell.hs
@@ -2,14 +2,15 @@ import Codec.Picture
 import GHC.Exts
 import Data.Maybe
 import Debug.Trace
-type Vector3 = ( Double, Double, Double )
+import Linear
+type Vector3 = V3 Double
 type Origin = Vector3
 type Normal = Vector3
 type Direction = Vector3
 type Location = Vector3
 type Radius = Double
 type FocalLength = Double
-type Color = (Int,Int,Int)
+type Color = V3 Int
 data Surface = Sphere Origin Radius ( Vector3 -> Color ) ( Light -> [Surface] -> Surface -> Vector3 -> Vector3 -> Vector3 -> Vector3 )
              | Plane Origin Normal  ( Vector3 -> Color ) ( Light -> [Surface] -> Surface -> Vector3 -> Vector3 -> Vector3 -> Vector3 )
 data Line = Line Origin Direction
@@ -36,47 +37,36 @@ _y_cam_coords c ar = _x_cam_coords c (-(1/ar))
 -- ar aspectratio x-size/y-size
 -- fl 10focallength distance between film and pinhole
 cameraRays:: Int -> Int -> Double -> Double -> [Vector3]
-cameraRays xc yc ar fl = [ ( x, y, (-fl) ) | y <- _y_cam_coords yc (1/ar), x <- _x_cam_coords xc ar ]
-defaultCamera focallength = Camera ( 0, 0, 0 ) (map normalize $ cameraRays 1280 720 1.78 focallength)
+cameraRays xc yc ar fl = [ (V3 x y (-fl)) | y <- _y_cam_coords yc (1/ar), x <- _x_cam_coords xc ar ]
+defaultCamera focallength = Camera (V3 0 0 0) (map normalize $ cameraRays 1280 720 1.78 focallength)
 
-absolute:: Vector3 -> Double
-absolute ( x, y, z ) = ( a + b + c ) ** ( 1/2 )
-        where a = x ** 2
-              b = y ** 2
-              c = z ** 2
-
-normalize:: Vector3 -> Vector3
-normalize ( x , y , z ) = ( x/l, y/l, z/l )
-        where l = absolute ( x, y, z )
+absolute = norm
 
 -- inproduct between vectors
 -- cosine of angle when used with normalized vectors
 -- AKA magnitude*|a||b| of projection of the smaller on the bigger vector
-(*.):: Vector3 -> Vector3 -> Double
-(*.) (a1, a2, a3) (b1, b2, b3) = (a1*b1)+(a2*b2)+(a3*b3)
+(*.) = dot
 
-(-.):: Vector3 -> Vector3 -> Vector3
-(-.) (a1, a2, a3) (b1, b2, b3) = ( a1 - b1, a2 - b2, a3 - b3 )
+(-.) = (-)
 
-(+.):: Vector3 -> Vector3 -> Vector3
-(+.) (a1, a2, a3) (b1, b2, b3) = ( a1 + b1, a2 + b2, a3 + b3 )
+(+.) = (+)
 
 sphereIntersectionFormula:: Vector3 -> Vector3 -> Surface -> Vector3
-sphereIntersectionFormula support direction (Sphere location radius _ _) = ( a, b, c )
+sphereIntersectionFormula support direction (Sphere location radius _ _) = (V3 a b c)
     where a = 1
           b = 2 * ( direction *. ( support -. location ))
           c = ((absolute ( support -. location )) ** 2) - ( radius ** 2 )
 
 calcD:: Vector3 -> Double
-calcD ( a, b , c) = (b ** 2) - ( 4 * a * c )
+calcD (V3 a b c) = (b ** 2) - ( 4 * a * c )
 
 intersection:: Line -> Surface -> Maybe Double
 intersection (Line support direction) (Sphere origin radius color shader)
     | d < 0 = Nothing
     | x1 < 0 && x2 < 0 = Nothing
     | otherwise = Just $ min x1 x2
-    where (a, b, c) = sphereIntersectionFormula support direction (Sphere origin radius color shader)
-          d = calcD (a, b, c)
+    where (V3 a b c) = sphereIntersectionFormula support direction (Sphere origin radius color shader)
+          d = calcD (V3 a b c)
           x1 = ((-b) + (d ** (1/2))) / (2*a)
           x2 = ((-b) - (d ** (1/2))) / (2*a)
 
@@ -88,7 +78,7 @@ intersection (Line support direction) (Plane origin normal color shader)
 
 
 sm:: Double -> Vector3 -> Vector3
-sm scalar (v1, v2, v3) = ( scalar*v1, scalar*v2, scalar*v3)
+sm = (*^)
 
 distance2Coord:: Line -> Double -> Vector3
 distance2Coord (Line origin direction) distance = origin +. ( distance `sm` direction )
@@ -105,21 +95,21 @@ cap i
     | otherwise = i
 
 flatQuantize:: Double -> Vector3 -> PixelRGBA8
-flatQuantize base ( a, b, c ) = PixelRGBA8 (flat a) (flat b) (flat c) 255
+flatQuantize base (V3 a b c) = PixelRGBA8 (flat a) (flat b) (flat c) 255
     where flat x = round(255*(x/base))
 
 expQuantize:: Double -> Double -> Vector3 -> PixelRGBA8
-expQuantize exposure base ( a, b, c ) = PixelRGBA8 (expose a) (expose b) (expose c) 255
+expQuantize exposure base (V3 a b c) = PixelRGBA8 (expose a) (expose b) (expose c) 255
     where expose x = round (( 1 - exp (-(x/base) * exposure )) * 255)
 
 getBase:: [Vector3] -> Double
 getBase pixels = base
-    where ( rs, gs, bs ) = unzip3 pixels
+    where ( rs, gs, bs ) = unzip3 $ map (\(V3 a b c) -> (a, b, c)) pixels
           base = max (maximum rs) $ max (maximum gs) (maximum bs)
 
 castRay :: Light -> [Surface] -> Camera -> Vector3 -> Vector3
 castRay l surfaces (Camera location _) ray
-    | null intersections = ( 0, 0, 10 )
+    | null intersections = (V3 0 0 10)
     | otherwise = case s of (Sphere _ _ _ shader) -> shader l surfaces s coord location ray
                             (Plane  _ _ _ shader) -> shader l surfaces s coord location ray
     where intersections = sortWith (\(_,d) -> fromJust d ) $ filter (\(_,d) -> d /= Nothing ) $ map (\s -> (s, intersection (Line location ray) s)) surfaces
@@ -128,9 +118,9 @@ castRay l surfaces (Camera location _) ray
 
 diffuseShader :: Double -> Light -> [Surface] -> Surface -> Vector3 -> Vector3 -> Vector3 -> Vector3
 diffuseShader albedo l surfaces s coord location ray
-    | factor < 0 = ( 0, 0, 0 )
-    | shadow = ( 0, 0, 0 )
-    | otherwise = ( sr, sg, sb )
+    | factor < 0 = (V3 0 0 0)
+    | shadow = (V3 0 0 0)
+    | otherwise = (V3 sr sg sb)
     where lv = case l of (DirectionalLight lv _) -> lv
                          (PointLight location _) -> location -. coord
           snv = surfaceNormal s coord
@@ -139,8 +129,8 @@ diffuseShader albedo l surfaces s coord location ray
           sr = fromIntegral(r)*factor
           sg = fromIntegral(g)*factor
           sb = fromIntegral(b)*factor
-          ( r, g, b ) = case s of (Sphere _ _ c _) -> c coord
-                                  (Plane _ _ c _ ) -> c coord
+          (V3 r g b) = case s of (Sphere _ _ c _) -> c coord
+                                 (Plane _ _ c _ ) -> c coord
           shadow = case l of (PointLight _ _) -> not $ null $ filter (\x -> x < absolute lv) $ catMaybes $ map (intersection (Line coord_bias (normalize lv))) surfaces
                              (DirectionalLight _ _) -> not $ null $ catMaybes $ map (intersection (Line coord_bias (normalize lv))) surfaces
           coord_bias = (1e-7 `sm` snv ) +. coord
@@ -149,9 +139,9 @@ defaultDiffuseShader = (diffuseShader pi)
 
 nullShader :: Light -> [Surface] -> Surface -> Vector3 -> Vector3 -> Vector3 -> Vector3
 nullShader _ _ s coord _ _
-    = (fromIntegral(r), fromIntegral(g), fromIntegral(b)) 
-    where (r, g, b) = case s of (Sphere _ _ c _) -> c coord
-                                (Plane _ _ c _ ) -> c coord
+    = (V3 (fromIntegral(r)) (fromIntegral(g)) (fromIntegral(b)))
+    where (V3 r g b) = case s of (Sphere _ _ c _) -> c coord
+                                 (Plane _ _ c _ ) -> c coord
 
 reflectionShader :: Light -> [Surface] -> Surface -> Vector3 -> Vector3 -> Vector3 -> Vector3
 reflectionShader l surfaces s coord location ray
@@ -159,8 +149,8 @@ reflectionShader l surfaces s coord location ray
     where rv = ( ray -. ( ( 2 * ( ray *. snv ) ) `sm` snv ) )
           snv = surfaceNormal s coord
           coord_bias = (1e-7 `sm` snv ) +. coord
-          (r, g, b) = case s of (Sphere _ _ c _) -> c coord
-                                (Plane _ _ c _ ) -> c coord
+          (V3 r g b) = case s of (Sphere _ _ c _) -> c coord
+                                 (Plane _ _ c _ ) -> c coord
 
 schlickShader :: Double -> Light -> [Surface] -> Surface -> Vector3 -> Vector3 -> Vector3 -> Vector3
 schlickShader r0 l surfaces s coord location ray
@@ -192,9 +182,9 @@ getColor s coord = case s of (Sphere _ _ c _) -> c coord
                              (Plane _ _ c _ ) -> c coord
 
 taint:: Color -> Vector3 -> Vector3
-taint color shade = (component c1 s2, component c2 s2, component c3 s3)
-    where (c1, c2, c3) = color
-          (s1, s2, s3) = shade
+taint color shade = (V3 (component c1 s2) (component c2 s2) (component c3 s3))
+    where (V3 c1 c2 c3) = color
+          (V3 s1 s2 s3) = shade
           component c s = s*fromIntegral(c)/255
 
 reflectanceFromRefractionIndex n1 = r0
@@ -208,18 +198,18 @@ concrete_img = map (expQuantize 8 base) img
 (_, expimg) = generateFoldImage (getPix) concrete_img 1280 720
 
 checker:: Color -> Color -> Double -> Vector3 -> Color
-checker black white size ( x, _, z )
+checker black white size (V3 x _ z)
     | ( floor(x/size) + floor(z/size) ) `mod` 2 == 0 = black
-    | otherwise                                        = white
+    | otherwise                                      = white
 
 -- scene, light and camera
-sphere = Sphere ( 15, 0, -60) 15 (\_ -> (255, 215, 0)) (schlickMetalShader 0.229 6.79)
-sphere2 = Sphere ( -15, 0, -45) 15 (\_ -> (255, 215, 1)) (schlickShader $ reflectanceFromRefractionIndex 1.54)
-plane = Plane (0, -15, 0 ) ( 0, 1, 0 ) (checker (32, 32, 32) (127, 127, 127) 10) defaultDiffuseShader
+sphere = Sphere (V3  15 0 (-60)) 15 (\_ -> (V3 255 215 0)) (schlickMetalShader 0.229 6.79)
+sphere2 = Sphere (V3 (-15) 0 (-45)) 15 (\_ -> (V3 255 215 1)) (schlickShader $ reflectanceFromRefractionIndex 1.54)
+plane = Plane (V3 0 (-15) 0 ) (V3 0 1 0 ) (checker (V3 32 32 32) (V3 127 127 127) 10) defaultDiffuseShader
 scene = [ sphere, plane, sphere2 ]
 camera = defaultCamera 1
-light = PointLight (30, 30, 0) 0.1e5
---light = DirectionalLight ( 1, 1, 1) 0.2e5
+light = PointLight (V3 30 30 0) 0.1e5
+--light = DirectionalLight (V3  1 1 1) 0.2e5
 Camera _ rays = camera
 
 main :: IO()
